93dcec4d35dd05f5577125631f8619197bdaccb1
Slava Ustovytski <slava.ustovytski@gmail.com>
2014-09-24 07:05:39 +1000

    problem 1. slight improments.

--- problem-1.go
+++ problem-1.go
@@ -1,14 +1,13 @@
 package main
 
 import "fmt"
-import "math"
 
 func main() {
-	var sum float64 = 0.0
-	for i := 1.0; i < 1000; i++ {
-		if math.Mod(i, 3) == 0 || math.Mod(i, 5) == 0 {
+	sum := 0
+	for i := 1; i < 1000; i++ {
+		if i%3 == 0 || i%5 == 0 {
 			sum += i
 		}
 	}
-	fmt.Println(sum)
+	fmt.Printf("Sum is: %v \n", sum)
 }
